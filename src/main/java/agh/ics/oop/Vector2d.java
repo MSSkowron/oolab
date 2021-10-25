d974b520ee6b57104828bd793923f3c591f1c7ab
MSSkowron <mateusz.skowron@poczta.onet.pl>
2021-10-25 13:33:48 +0200

    "NewLab1"

--- src/main/java/agh/ics/oop/Vector2d.java
+++ src/main/java/agh/ics/oop/Vector2d.java
@@ -1,5 +1,7 @@
 package agh.ics.oop;
 
+import java.util.Objects;
+
 public class Vector2d {
     public final int x;
     public final int y;
@@ -15,47 +17,35 @@ public class Vector2d {
     }
 
     public boolean precedes(Vector2d other) {
-        if (other == null) {
-            throw new IllegalArgumentException("Wrong argument!");
-        }
         return this.x <= other.x && this.y <= other.y;
     }
 
     public boolean follows(Vector2d other) {
-        if (other == null) {
-            throw new IllegalArgumentException("Wrong argument!");
-        }
         return this.x >= other.x && this.y >= other.y;
     }
 
     public Vector2d upperRight(Vector2d other) {
-        if (other == null) {
-            throw new IllegalArgumentException("Wrong argument!");
-        }
         return new Vector2d(Math.max(this.x, other.x), Math.max(this.y, other.y));
     }
 
     public Vector2d lowerLeft(Vector2d other) {
-        if (other == null) {
-            throw new IllegalArgumentException("Wrong argument!");
-        }
         return new Vector2d(Math.min(this.x, other.x), Math.min(this.y, other.y));
     }
 
     public Vector2d add(Vector2d other) {
-        if (other == null) {
-            throw new IllegalArgumentException("Wrong argument!");
-        }
         return new Vector2d(this.x + other.x, this.y + other.y);
     }
 
     public Vector2d subtract(Vector2d other) {
-        if (other == null) {
-            throw new IllegalArgumentException("Wrong argument!");
-        }
         return new Vector2d(this.x - other.x, this.y - other.y);
     }
 
+    @Override
+    public int hashCode() {
+        return Objects.hash(x, y);
+    }
+
+    @Override
     public boolean equals(Object other) {
         if (this == other) return true;
         if (!(other instanceof Vector2d vector2d)) return false;
